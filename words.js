3418ec4a4fe618cbb1345ed5ca63c9fbae9122f2
JatayuDuta <150224859+JatayuDuta@users.noreply.github.com>
2023-12-01 00:24:46 +0700

    Add files via upload

---
+++ words.js
@@ -0,0 +1,58 @@
+let words = [
+    {
+        word: "pernyataan",
+        hint: "tindakan yang dilakukan untuk menyatakan pendapat"
+    },
+    {
+        word: "telinga",
+        hint: "dapat dipegang tapi tidak dapat dilihat"
+    },
+    {
+        word: "judi",
+        hint: "menjanjikan kemenangan"
+    },
+    {
+        word: "ayang",
+        hint: "obat meriang"
+    },
+    {
+        word: "sikut",
+        hint: "dapat dipegang tapi tidak dapat dicium"
+    },
+    {
+        word: "saya",
+        hint: "beban orang tua"
+    },
+    {
+        word: "kamu",
+        hint: "manusia paling ngangenin"
+    },
+    {
+        word: "megawati",
+        hint: "pemimpin para banteng"
+    },
+    {
+        word: "kung-flu",
+        hint: "Nama penyakit paling populer di China"
+    },
+    {
+        word: "penyanyi",
+        hint: "Bermain saat kerja"
+    },
+    {
+        word: "atlet",
+        hint: "Bermain saat kerja"
+    },
+    {
+        word: "lemaribuan",
+        hint: "Lemari yang bisa dimasukkan ke kantong"
+    },
+    {
+        word: "peti mati",
+        hint: "baru dibeli langsung dibuang"
+    },
+    {
+        word: "pinjam 100",
+        hint: "agar silaturahmi tidak putus"
+    },
+]
